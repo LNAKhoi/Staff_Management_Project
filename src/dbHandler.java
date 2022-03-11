86bbdf8ab96521d8b63a0872019fafc98224e664
Jason Koi <lnakhoi1811@gmail.com>
2022-03-12 01:13:00 +0700

    find Staff function

--- src/dbHandler.java
+++ src/dbHandler.java
@@ -147,4 +147,39 @@ public class dbHandler {
             e.printStackTrace();
         }
     }
+    public static Staff getAStaff(int ID){
+        Staff returnStaff=null;
+        // Connect to Database
+        Connection con=null;
+        Statement statement=null;
+        try {
+            /*
+             * In this project, we will use MySQL in Xampp to manage the database
+             * port :3306
+             * */
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/staff_management","root","");
+            String query= "Select * From Staff";
+            statement=con.createStatement();
+            ResultSet resultSet= statement.executeQuery(query);
+            while(resultSet.next()){
+                if(ID==resultSet.getInt("ID")){
+                    returnStaff=new Staff(resultSet.getInt("ID"),resultSet.getString("Name"),
+                            resultSet.getDate("DOB"),resultSet.getFloat("Salary"), resultSet.getString("Gender"),
+                            resultSet.getString("Address"),resultSet.getString("Position"),resultSet.getDate("StartDate"),resultSet.getString("imgPath"));
+                }
+            }
+            if(con!=null){
+                System.out.println("Connection available!");
+                con.close();
+            }
+            if(statement!=null){
+                System.out.println("Valid statement!");
+                statement.close();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return returnStaff;
+    }
 }
